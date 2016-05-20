212b18a3be5b3f03e185deb1021e9aca06e205b9
Jessy Barthelemy <jessy-barthelemy@hotmail.fr>
2016-05-20 14:07:51 +0200

    amelioration du profil

--- profil.php
+++ profil.php
@@ -13,33 +13,52 @@
                         <img src="http://placehold.it/50x50" alt="Badge 3">
                         <img src="http://placehold.it/50x50" alt="Badge 4">
                     </div>
+                    
+                    <h3>Uvs</h3>
+                    <div>
+                        <img src="http://placehold.it/50x50" alt="Badge 1">
+                        <img src="http://placehold.it/50x50" alt="Badge 2">
+                        <img src="http://placehold.it/50x50" alt="Badge 3">
+                        <img src="http://placehold.it/50x50" alt="Badge 4">
+                    </div>
                 </div>
             </div>
             
             <div class="col s10 m8 offset-m2 offset-s1">
-            <h3>Commentaires</h3>
-            <!-- Single comment -->
-            <div class="flex comment grey lighten-4">
-                <div>
-                    <a href="profil.php">J.Barthelemy</a>
-                    <p>
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam pulvinar malesuada rhoncus. Integer consequat semper tincidunt.
-                    </p>
+                <h3>Notifications</h3>
+                <form action="" method="POST" id="preferences">
+                    <div class="switch">
+                        <label>Suggestions d'annonces</label>
+                        <label>
+                          <input type="checkbox">
+                          <span class="lever"></span>
+                        </label>
+                    </div>
+                </form>
+                <h3>Commentaires</h3>
+                <!-- Single comment -->
+                <div class="flex comment grey lighten-4">
+                    <div>
+                        <a href="profil.php">J.Barthelemy</a>
+                        <p>
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam pulvinar malesuada rhoncus. Integer consequat semper tincidunt.
+                        </p>
+                    </div>
+                    <div class="clight-blue notation-medium">&#9733;&#9733;&#9733;&#9734;&#9734;</div>
                 </div>
-                <div class="clight-blue notation-medium">&#9733;&#9733;&#9733;&#9734;&#9734;</div>
-            </div>
 
-            <!-- Single comment -->
-            <div class="flex comment odd">
-                <div>
-                    <a href="profil.php">J.Barthelemy</a>
-                    <p>
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam pulvinar malesuada rhoncus. Integer consequat semper tincidunt.
-                    </p>
+                <!-- Single comment -->
+                <div class="flex comment odd">
+                    <div>
+                        <a href="profil.php">J.Barthelemy</a>
+                        <p>
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam pulvinar malesuada rhoncus. Integer consequat semper tincidunt.
+                        </p>
+                    </div>
+                    <div class="clight-blue notation-medium">&#9733;&#9733;&#9733;&#9734;&#9734;
+                    </div>
                 </div>
-                <div class="clight-blue notation-medium">&#9733;&#9733;&#9733;&#9734;&#9734;</div>
             </div>
-        </div>
         </div>
     </div>
 <?php  include("footer.php"); ?>1
